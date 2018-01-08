793132f8f412ed8fafa7c3a233e44fbdeabb5b41
Josh <mcnett88@gmail.com>
2018-01-08 14:20:48 -0500

    updated mobile route with updated functions

--- app.rb
+++ app.rb
@@ -34,7 +34,13 @@ end
 get '/hidden_page' do
 	upc = params[:upc]
   html_info = get_upc_info(upc)
-  erb :results, locals: {product_info: html_info}
+  print("HTML_INFO CLASS: #{html_info.class}")
+  product_title = get_product_title(html_info)
+  product_price = get_product_price(html_info)
+  large_photos_array = get_large_images(html_info)
+  # print(html_info)
+  # redirect '/product?results='+results
+  erb :results, locals: {product_info: html_info, large_photos_array: large_photos_array, product_title: product_title, product_price: product_price}
 end
 
 #this is just to trigger gits change monitor 
