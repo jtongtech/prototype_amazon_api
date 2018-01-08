f0270a17fbe4c4097e59cad02971262588536071
Josh <mcnett88@gmail.com>
2018-01-08 11:09:18 -0500

    have functionality to search upc from webview and show the raw result currently, also have route for mobile search

--- api.rb
+++ api.rb
@@ -6,51 +6,53 @@ require 'openssl'
 require 'base64'
 require 'open-uri'
 require 'nokogiri'
+load './local_env.rb' if File.exist?('./local_env.rb')
 
 # Your Access Key ID, as taken from the Your Account page
 ACCESS_KEY_ID = ENV['AWS_ACCESS_KEY_ID']
 
 # Your Secret Key corresponding to the above ID, as taken from the Your Account page
-SECRET_KEY = ENV['SECRET_KEY']
+SECRET_KEY = ENV['AWS_SECRET_ACCESS_KEY']
 
 # The region you are interested in
 ENDPOINT = "webservices.amazon.com"
 
 REQUEST_URI = "/onca/xml"
 
-def get_upc
-  upc = params[:upc]
+def get_upc_info(upc)
+
+  params = {
+    "Service" => "AWSECommerceService",
+    "Operation" => "ItemLookup",
+    "AWSAccessKeyId" => ENV['AWS_ACCESS_KEY_ID'], #from locals
+    "AssociateTag" => ENV['AWS_SECRET_ACCESS_KEY'], #from locals
+    "ItemId" => upc, #variable from user input
+    "IdType" => "UPC", #variable from dropdown
+    "ResponseGroup" => "Images,ItemAttributes,ItemIds",
+    "SearchIndex" => "All"
+  }
+
+  # Set current timestamp if not set
+  params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
+
+  # Generate the canonical query
+  canonical_query_string = params.sort.collect do |key, value|
+    [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
+  end.join('&')
+
+  # Generate the string to be signed
+  string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
+
+  # Generate the signature required by the Product Advertising API
+  signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), SECRET_KEY, string_to_sign)).strip()
+
+  # Generate the signed URL
+  request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
+
+  html_result = Nokogiri::HTML(open(request_url))
+  # monkeys.css('largeimage').each do |monkey|
+  puts "THIS IS html_result: #{html_result}"
+  # end
+  # puts "Signed URL: \"#{request_url}\""
+  html_result
 end
-
-
-params = {
-  "Service" => "AWSECommerceService",
-  "Operation" => "ItemLookup",
-  ACCESS_KEY_ID = ENV['AWS_ACCESS_KEY_ID'], #from locals
-  AWS_AFFILIATE_KEY = ENV['AWS_AFFILIATE_KEY'], #from locals
-  "ItemId" => upc, #variable from user input
-  "IdType" => "UPC", #variable from dropdown
-  "ResponseGroup" => "Images,ItemAttributes,ItemIds",
-  "SearchIndex" => "All"
-}
-
-# Set current timestamp if not set
-params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
-
-# Generate the canonical query
-canonical_query_string = params.sort.collect do |key, value|
-  [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-end.join('&')
-
-# Generate the string to be signed
-string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
-
-# Generate the signature required by the Product Advertising API
-signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), SECRET_KEY, string_to_sign)).strip()
-
-# Generate the signed URL
-request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
-
-monkeys = Nokogiri::HTML(open(request_url))
-puts monkeys
-# puts "Signed URL: \"#{request_url}\""
--- app.rb
+++ app.rb
@@ -7,14 +7,29 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 load './local_env.rb' if File.exist?('./local_env.rb')
 
 
-get '/get_info' do
-	upc = params[:upc]
+
+get '/' do
+	# upc_info = params[:html_info]
+	# print(upc_info)
 	erb :input
 end
 
 post '/get_info' do
   upc = params[:upc]
-  redirect '/product?results='+results
+  html_info = get_upc_info(upc)
+  # print(html_info)
+  # redirect '/product?results='+results
+  erb :results, locals: {product_info: html_info}
 end  
+
+post '/return_home' do
+	redirect '/'
+end
+
+get '/hidden_page' do
+	upc = params[:upc]
+  html_info = get_upc_info(upc)
+  erb :results, locals: {product_info: html_info}
+end
 
 #this is just to trigger gits change monitor 
