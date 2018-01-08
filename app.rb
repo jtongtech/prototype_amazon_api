762f76e5572436d45bf65b4f79b0e91b17ba9b12
Josh <mcnett88@gmail.com>
2018-01-08 16:38:44 -0500

    updated mobile route

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ post '/get_info' do
   html_info = get_upc_info(upc)
   # print("HTML_INFO!!!!!!!!!!!!!!!!!!!!!!!: #{html_info}")
   error_check = error_check(html_info)
-  puts("error_check is: #{error_check}")
+  # puts("error_check is: #{error_check}")
   if error_check == ''
     product_title = get_product_title(html_info)
     product_price = get_product_price(html_info)
@@ -42,13 +42,21 @@ end
 get '/hidden_page' do
 	upc = params[:upc]
   html_info = get_upc_info(upc)
-  
-  product_title = get_product_title(html_info)
-  product_price = get_product_price(html_info)
-  large_photos_array = get_large_images(html_info)
-  # print(html_info)
-  # redirect '/product?results='+results
-  erb :results, locals: {product_info: html_info, large_photos_array: large_photos_array, product_title: product_title, product_price: product_price}
+  # print("HTML_INFO!!!!!!!!!!!!!!!!!!!!!!!: #{html_info}")
+  error_check = error_check(html_info)
+  # puts("error_check is: #{error_check}")
+  if error_check == ''
+    product_title = get_product_title(html_info)
+    product_price = get_product_price(html_info)
+    large_photos_array = get_large_images(html_info)
+    # print(html_info)
+    # redirect '/product?results='+results
+  else
+    product_title = ''
+    product_price = ''
+    large_photos_array = ['']
+  end
+    erb :results, locals: {product_info: html_info, large_photos_array: large_photos_array, product_title: product_title, product_price: product_price, error: error_check}
 end
 
 #this is just to trigger gits change monitor 
