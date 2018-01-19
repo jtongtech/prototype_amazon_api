411a8c9abbbb52c265c87eee40f5590224aefe13
Josh <mcnett88@gmail.com>
2018-01-19 11:25:42 -0500

    added functionality for dsld api to seperate statments by statement type for storage in database.

--- dsld_api.rb
+++ dsld_api.rb
@@ -30,7 +30,7 @@ def dsld_api(url)
     dsld_data_hash = JSON.parse(response.read_body)
     # dsld_id = dsld_data_hash["foundin_05_anyhwere"]["products"][0]["ID"]
     # puts dsld_id
-    # puts(dsld_data_hash, "hash is here")
+     # puts(dsld_data_hash, "hash is here")
     dsld_data_hash
 
 end
@@ -63,12 +63,94 @@ def get_dsld_lable_info(dsld_id)
     dsld_info
 end
 
-def get_statement_of_identity(dsld_info)
-    # id = get_id_from_result(dsld_info)
-    # result = get_dsld_lable_info(id)
-    statement_of_identity = dsld_info["Statement_of_Identity"]
-    statement_of_identity
+def get_statements_of_formulation(dsld_info)
+    formulation_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Formulation"
+            formulation_statments << statement["Statement"]
+        end
+    end
+    # puts(formulation_statments)
+    formulation_statments
 end
+
+def get_statements_of_other(dsld_info)
+    other_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Other"
+            other_statments << statement["Statement"]
+        end
+    end
+    # puts(other_statments)
+    other_statments
+end
+
+def get_statements_of_recommended(dsld_info)
+    recommended_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Suggested/Recommended/Usage/Directions"
+            recommended_statments << statement["Statement"]
+        end
+    end
+    # puts(recommended_statments)
+    recommended_statments
+end
+
+def get_statements_of_disclaimer_statement(dsld_info)
+    disclaimer_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "FDA Disclaimer Statement"
+            disclaimer_statments << statement["Statement"]
+        end
+    end
+    # puts(disclaimer_statments)
+    disclaimer_statments
+end
+
+def get_statements_of_identity_statement(dsld_info)
+    identity_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "FDA Statement of Identity"
+            identity_statments << statement["Statement"]
+        end
+    end
+    # puts(identity_statments)
+    identity_statments
+end
+
+def get_statements_of_precautions(dsld_info)
+    precautions_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Precautions"
+            precautions_statments << statement["Statement"]
+        end
+    end
+    # puts(precautions_statments)
+    precautions_statments
+end
+
+def get_statements_of_prod_specific_info(dsld_info)
+    prod_specific_info_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Product Specific Information"
+            prod_specific_info_statments << statement["Statement"]
+        end
+    end
+    # puts(prod_specific_info_statments)
+    prod_specific_info_statments
+end
+
+def get_statements_of_seals_and_symbols(dsld_info)
+    seals_and_symbols_statments = []
+    dsld_info["statements"].each do |statement|
+        if statement["Statement_Type"] == "Seals/Symbols"
+            seals_and_symbols_statments << statement["Statement"]
+        end
+    end
+    # puts(seals_and_symbols_statments)
+    seals_and_symbols_statments
+end
+
 
 def get_product_name(dsld_info)
     # id = get_id_from_result(dsld_info)
@@ -84,28 +166,50 @@ def get_suggested_use(dsld_info)
 end
 
 def get_statments(dsld_info)
-##### FUNCTION NOT FINISHED DETAILS COME BACK BUT ARE MIXED ##########
-    result = complete_dsld_api_result(get_dsld_id)
+    # result = complete_dsld_api_result(get_dsld_id)
     statements = []
-    result["statements"].each do |statement|
-        puts(statement["Statement"])
+    # puts(dsld_info["statements"])
+    dsld_info["statements"].each do |statement|
+        statements << statement["Statement"]
+         puts(statement["Statement_Type"])
+        # puts(statement["Statement"])
     end
+    statements
 end
 
-new_upc = add_spacing_to_upc("030768032682")
+def get_ingredients(dsld_info)
+    ingredients = []
+    dsld_info["ingredients"].each do |ingredient|
+        ingredients << [ingredient["Dietary_Ingredient_Synonym_Source"], ingredient["Amount_Per_Serving"], ingredient["Amount_Serving_Unit"]]
+        # puts(ingredient["Dietary_Ingredient_Synonym_Source"])
+    end
+    ingredients
+end
+
+# new_upc = add_spacing_to_upc("719985300400")
+new_upc = add_spacing_to_upc("733739003409")
+# new_upc = add_spacing_to_upc("037000741343")
 # print(new_upc, "this is the new upc")
 
-result = get_dsld_api_result("0 30768 03268 2")
+result = get_dsld_api_result(new_upc)
 # print(result, "HERE IS THE RESULT")
-get_id_from_result(result)
+id = get_id_from_result(result)
 # print(result, "this is result")
-# dsld_hash = get_dsld_lable_info(result)
+dsld_hash = get_dsld_lable_info(id)
 # print(dsld_hash)
-
-# print(statements)
-
-
-
-# result.each do |info|
-#     puts info
-# end
+statements = get_statments(dsld_hash)
+suggested_use = get_suggested_use(dsld_hash)
+product_name = get_product_name(dsld_hash)
+statements_of_formulation = get_statements_of_formulation(dsld_hash)
+statements_of_other = get_statements_of_other(dsld_hash)
+statements_of_recommended = get_statements_of_recommended(dsld_hash)
+statements_of_disclaimer_statement = get_statements_of_disclaimer_statement(dsld_hash)
+statements_of_identity = get_statements_of_identity_statement(dsld_hash)
+statements_of_precautions = get_statements_of_precautions(dsld_hash)
+statements_of_prod_specific_info = get_statements_of_prod_specific_info(dsld_hash)
+statements_of_seals_and_symbols = get_statements_of_seals_and_symbols(dsld_hash)
+ingredient = get_ingredients(dsld_hash)
+# puts(suggested_use, "<----- SUGGESTED USE")
+# puts(product_name, "<----- PRODUCT TITLE")
+# puts(statement_of_identity, "<----- STATEMENT OF IDENTITY")
+# puts(ingredient, "<----- THESE ARE INGREDIENTS")
